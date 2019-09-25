bfe743baed7534dd67e17aba9e2a66d711f3e412
istmejitu <mikal.jitu@gmail.com>
2019-09-25 21:05:01 +0700

    added password

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -12,4 +12,6 @@ VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
             uniqueness:{case_sensitive: false},
             length: {minimum: 3, maximum: 50},
             format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
