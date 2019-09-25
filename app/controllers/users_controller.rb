23fcdabf383d245b14e5a8caa04d497c341acb49
istmejitu <mikal.jitu@gmail.com>
2019-09-26 00:39:52 +0700

    added create and edit user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,25 @@ class UsersController <ApplicationController
       render 'new'
     end
   end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+@user = User.find(params[:id])
+  if @user.update(user_params)
+    flash[:notice] = "User was successfully updated"
+    redirect_to user_path(@user)
+  else
+    render 'edit'
+  end
+end
+
+def show
+  @user = User.find(params[:id])
+end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
