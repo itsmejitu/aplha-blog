22104d01c73fe77a26330281e7bec7c22ad57365
istmejitu <mikal.jitu@gmail.com>
2019-09-25 18:47:02 +0700

    user validations are added

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,4 +1,11 @@
 class User <ApplicationRecord
-  validates :username, presence: true, length: {minimum: 3, maximum: 50}
-  validates :email, presence: true, length: {minimum:5, maximum: 50}
+  validates :username,presence: true,
+            uniqueness:{case_sensitive: false},
+            length: {minimum: 3, maximum: 50}
+            
+VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email,presence: true,
+            uniqueness:{case_sensitive: false},
+            length: {minimum: 3, maximum: 50},
+            format: { with: VALID_EMAIL_REGEX }
 end
